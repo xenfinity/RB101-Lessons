bf1693cc610ec2b9d9159b166c3ec9bd64ab502e
Isaac Dutton <k1p3psyygojb@nowbuzzoff.com>
2021-11-22 11:16:59 -0500

    formatting

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -31,18 +31,17 @@ loop do
     years = gets.chomp
     valid_number?(years) ? break : prompt(MESSAGES['invalid'])
   end
-  monthly_rate = (apr.to_f/12)/100
-  monthly_payment = loan_amount.to_f * 
+  monthly_rate = (apr.to_f / 12) / 100
+  monthly_payment = loan_amount.to_f *
                     (monthly_rate / 
-                    (1-(1+monthly_rate) ** (-years.to_i*12)))
+                      (1 - (1 + monthly_rate)**(-years.to_i * 12))
+                    )
 
   prompt("Monthly Payment:  $#{monthly_payment.round(2)}")
 
   prompt(MESSAGES['again?'])
-  next if gets.chomp.downcase == 'y' 
+  next if gets.chomp.downcase == 'y'
   break
 end
 
 prompt(MESSAGES['goodbye'])
-
-
